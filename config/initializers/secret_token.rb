d339bcc148800fd52441425085ffdde880573798
EBS <lissaschmidt@yahoo.com>
2012-06-07 11:10:14 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '66bb5f82cc6f47a8423e212067468f9235dfedf0d1d4a7b620c219f3955528e4f1f0c4c8ef06e34a33d26e4b3f9f392654aa642171e0406347bf63b3c2ba1217'
